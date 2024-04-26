835317937206971b85f7f78ec686c9e0bf840ff6
Daniel Martínez León <daniel1martinezleon@gmail.com>
2024-04-26 12:55:42 +0200

    Proyecto creado

---
+++ ProyectoFinalDanielMartinez/src/main/java/com/salesianostriana/dam/proyectofinaldanielmartinez/ProyectoFinalDanielMartinezApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectofinaldanielmartinez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalDanielMartinezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalDanielMartinezApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinalDanielMartinez/src/test/java/com/salesianostriana/dam/proyectofinaldanielmartinez/ProyectoFinalDanielMartinezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectofinaldanielmartinez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalDanielMartinezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
